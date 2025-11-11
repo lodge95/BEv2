041e3eada9e66b098fc584ef87fa810c7c492ef2
lodge95 <stanley.r.lodge@gmail.com>
2025-11-11 17:02:08 -0500

    Add main class for MyBizExchange Spring Boot app

---
+++ src/main/java/com/mybizexchange/MyBizExchangeApplication.java
@@ -0,0 +1,11 @@
+package com.mybizexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBizExchangeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyBizExchangeApplication.class, args);
+    }
+}
